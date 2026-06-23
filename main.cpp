0154508c50b9237caac48f002ad86bb417a962e4
prateek2413 <prateekbishnoi24@gmail.com>
2026-06-23 13:06:15 +0530

    error solved

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
 #include<string>
+#include<limits>
 using namespace std;
 class File{
     public:
@@ -22,19 +23,57 @@ class FileManager{
     private:
     vector<File> files;
     public:
+    bool idExists(int id){
+        for(int i = 0; i < files.size(); i++)
+        {
+            if(files[i].id == id){
+                return true;
+            }
+        }
+        return false;
+    }
     void addFile(){
         int id;
         string name;
         int size;
 
-        cout<<"\nEnter File ID: ";
-        cin>>id;
+        while(true){
+            cout<<"\nEnter File ID: ";
+            cin>>id;
+            if(cin.fail()){
+                cout<<"Invalid ID! Enter a number only.\n";
+                cin.clear();
+                cin.ignore(1000,'\n');
+                continue;
+            }
+            if(idExists(id)){
+                cout << "File ID already exists!\n";
+                continue;
+            }
+            break;
+        }
         
-        cout<<"Enter File Name: ";
-        cin>>name;
+        cin.ignore();
+        while(true){
+            cout<<"Enter File name: ";
+            getline(cin, name);
+            if(!name.empty()){
+                break;
+            }
+            cout<<"Name of File cannot be Empty\n";
+        }
 
-        cout<<"Enter File Size (KB): ";
-        cin>>size;
+        while(true){
+            cout<<"Enter File Size: ";
+            cin>>size;
+            if(cin.fail()){
+                cout<<"Invalid Size! Enter a number only.\n";
+                cin.clear();
+                cin.ignore(1000,'\n');
+                continue;
+            }
+            break;
+        }
 
         File newFile(id, name, size);
         files.push_back(newFile);
@@ -62,8 +101,17 @@ int main(){
         cout<<"2.Display Files\n";
         cout<<"3.Exit\n";
 
-        cout<<"Enter Choice: ";
-        cin>>choice;
+        while(true){
+            cout<<"Enter Choice: ";
+            cin>>choice;
+            if(cin.fail()){
+                cout<<"Invalid input! Please enter a number.\n";
+                cin.clear();
+                cin.ignore(1000,'\n');
+                continue;
+            }
+            break;
+        }
 
         switch(choice){
             case 1:
